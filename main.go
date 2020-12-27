8b81efb969322211417b688b482b1b7308a4dfb6
bokyun91 <braun@todayhenry.co>
2020-12-27 19:17:30 +0900

    setup echo 2

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/bokyun91/scrapper_nomad/scrapper"
+import (
+	"strings"
+
+	"github.com/bokyun91/scrapper_nomad/scrapper"
+	"github.com/labstack/echo"
+)
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+
+	return nil
+}
 
 func main() {
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
+
 }
